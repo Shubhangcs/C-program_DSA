f4e3567ac4eba63aad9072ba60c8c9225357b15a
Shubhanga CS <95484239+Shubhangcs@users.noreply.github.com>
2023-01-19 16:27:22 +0530

    Create vowelsorconsonants.c

---
+++ vowelsorconsonants.c
@@ -0,0 +1,44 @@
+// vowels and consonants using switch case
+#include<stdio.h>
+void main(){
+char a;
+printf("Enter any alphabet\n");
+scanf("%c",&a);
+switch(a)
+{
+	case 'A':
+		printf("%c is an Vowel\n",a);
+		break;
+	case 'E':
+		printf("%c is an Vowel\n",a);
+		break;
+	case 'I':
+		printf("%c is an Vowel\n",a);
+		break;
+	case 'O':
+		printf("%c is an Vowel\n",a);
+		break;
+	case 'U':
+		printf("%c is an Vowel\n",a);
+		break;
+	case 'a':
+		printf("%c is an Vowel\n",a);
+		break;
+	case 'e':
+		printf("%c is an Vowel\n",a);
+		break;
+	case 'i':
+		printf("%c is an Vowel\n",a);
+		break;
+	case '0':
+		printf("%c is an Vowel\n",a);
+		break;
+	case 'u':
+		printf("%c is an Vowel\n",a);
+		break;
+	default : printf("Consonant\n");
+	
+
+}
+
+}
