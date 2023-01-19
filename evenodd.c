6758c5da001ac229918ca73374a2f8d9518d5459
Shubhanga CS <95484239+Shubhangcs@users.noreply.github.com>
2023-01-19 16:28:04 +0530

    Create evenodd.c

---
+++ evenodd.c
@@ -0,0 +1,13 @@
+// To check even or odd
+#include<stdio.h>
+void main(){
+int n ;
+printf("Enter any number = ");
+scanf("%d",&n);
+if(n % 2 == 0){
+	printf("%d is Even\n",n);
+}
+else{
+printf("%d is Odd\n",n);
+}
+}
